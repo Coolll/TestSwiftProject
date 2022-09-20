9e0969fa344e19a10db4596b829dc9a84c519f7a
Coolll <2502127576@qq.com>
2022-09-20 11:40:39 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios,'13.0'
+target 'TestSwiftProject' do
+pod 'AFNetworking','~>3.1.0'
+pod 'GRDB.swift','~>5.16.0'
+end
---
+++ TestSwiftProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSwiftProject
+//
+//  Created by 龙培 on 2022/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
